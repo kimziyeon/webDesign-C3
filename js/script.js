44c66cdcc7d5698cfc84686249a9304f0814ce7c
kimziyeon <aa40254037@gmail.com>
2024-11-14 18:02:32 +0900

    20241114 재연습

--- js/script.js
+++ js/script.js
@@ -1,15 +1,13 @@
 $(function () {
 
-    // 헤더
+
+    // nav
     $('nav>ul>li').mouseenter(function () {
         $(this).children('.sub').stop().slideDown();
     })
-
     $('nav>ul>li').mouseleave(function () {
         $(this).children('.sub').stop().slideUp();
     })
-
-    //헤더 포커스인
 
     $('nav>ul>li').focusin(function () {
         $(this).children('.sub').stop().slideDown();
@@ -19,30 +17,26 @@ $(function () {
     $('nav>ul>li').focusout(function () {
         $(this).children('.sub').stop().slideUp();
         $(this).children('a').removeClass('on');
-
     })
 
     $('.sub>li').focusin(function () {
         $(this).children('a').addClass('on');
     })
-
     $('.sub>li').focusout(function () {
         $(this).children('a').removeClass('on');
     })
 
 
 
-
     // 슬라이드
     function slide() {
-        $('.slide ul').animate({ left: '-800px' }, 1000, function () {
-            $(this).append(
-                $('.slide ul li').first())
-            $('.slide ul').css({ left: '0' });
-        });
+
+        $('.slide>ul').animate({ left: '-800px' }, 1000, function () {
+            $(this).append($('.slide>ul>li').first());
+            $('.slide>ul').css({ left: '0' });
+        })
     }
     setInterval(slide, 3000)
-
 
 
     // 팝업
@@ -52,5 +46,6 @@ $(function () {
     $('.pop_off').click(function () {
         $('.pop').hide();
     })
+
 
 })
